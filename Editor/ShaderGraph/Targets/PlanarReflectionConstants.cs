1bf410b30b204dd1eacc3a65ee9235a33498c498
Eduardo Jimenez <barkley@siesta-games.com>
2025-10-07 14:02:09 +0200

    Moved the default path to be the package one (with a #define to set the local one)

--- Editor/ShaderGraph/Targets/PlanarReflectionConstants.cs
+++ Editor/ShaderGraph/Targets/PlanarReflectionConstants.cs
@@ -2,6 +2,10 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
 {
     public class PlanarReflectionConstants
     {
+#if USE_LOCAL_PATH_TO_PLANAR_REFLECTION_HLSL
         public const string PathToHLSL = "Assets/Plugins/URP-PlanarReflections/Editor/ShaderGraph/Includes/";
+#else
+        public const string PathToHLSL = "Packages/com.siestagames.urpplanarreflections/Editor/ShaderGraph/Includes/";
+#endif
     }
 }
